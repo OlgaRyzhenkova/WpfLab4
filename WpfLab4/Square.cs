732d4998a8f6dac44d9af07eb35f46a3813e4a07
Ryzhenkova Olga <ryzhenkova.olha1124@vu.cdu.edu.ua>
2025-12-16 21:59:53 +0200

    Перевизначення позиціонування

--- WpfLab4/Square.cs
+++ WpfLab4/Square.cs
@@ -6,5 +6,26 @@ namespace WpfLab4
 {
     public class Square : Figure
     {
+        public double SideLength;
+        public Square(double x, double y, double side)
+        {
+            CenterX = x;
+            CenterY = y;
+            SideLength = side;
+
+            Rectangle rect = new Rectangle();
+            rect.Width = side;
+            rect.Height = side;
+            rect.StrokeThickness = 2;
+
+            WpfShape = rect;
+            UpdatePosition();
+        }
+        public override void UpdatePosition()
+        {
+            Canvas.SetLeft(WpfShape, CenterX - SideLength / 2);
+            Canvas.SetTop(WpfShape, CenterY - SideLength / 2);
+        }
+
     }
 }
