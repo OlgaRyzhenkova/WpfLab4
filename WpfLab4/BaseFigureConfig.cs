9a15781265f77ba85283373826a8a2c9700c5415
Ryzhenkova Olga <ryzhenkova.olha1124@vu.cdu.edu.ua>
2025-12-16 22:07:20 +0200

    Бонусне завдання. Ми робимо "розумні" панелі налаштувань, які змінюються залежно від фігури.

---
+++ WpfLab4/BaseFigureConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace WpfLab4
+{
+    public abstract class BaseFigureConfig : UserControl
+    {
+        public abstract Figure CreateFigure();
+
+        protected double GetDouble(string text)
+        {
+            try 
+            { 
+                return Convert.ToDouble(text); 
+            } 
+            catch 
+            { 
+                return 0; 
+            }
+        }
+    }
+}
