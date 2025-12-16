8e9ad2d962d18dee0174e6970850f30ba75847a4
Ryzhenkova Olga <ryzhenkova.olha1124@vu.cdu.edu.ua>
2025-12-16 21:42:51 +0200

    створила клас Figure

--- WpfLab4/Figure.cs
+++ WpfLab4/Figure.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace WpfLab4
 {
-    internal class Figure
+    public abstract class Figure
     {
+
     }
 }
