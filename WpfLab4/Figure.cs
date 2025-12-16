0e48c7d071e7c900660ac3ab4b116a5122983283
Ryzhenkova Olga <ryzhenkova.olha1124@vu.cdu.edu.ua>
2025-12-16 21:48:23 +0200

    створила клас Figure

--- WpfLab4/Figure.cs
+++ WpfLab4/Figure.cs
@@ -6,6 +6,29 @@ namespace WpfLab4
 {
     public abstract class Figure
     {
+        public double CenterX;
+        public double CenterY;
 
+        // Картинка фігури (для Canvas)
+        public UIElement WpfShape;
+
+        public abstract void DrawBlack();
+        public abstract void HideDrawingBackGround();
+        public virtual void UpdatePosition()
+        {
+            if (WpfShape != null)
+            {
+                Canvas.SetLeft(WpfShape, CenterX);
+                Canvas.SetTop(WpfShape, CenterY);
+            }
+        }
+        public void Move(double dx, double dy)
+        {
+            HideDrawingBackGround();
+            CenterX += dx;        
+            CenterY += dy;         
+            UpdatePosition();      
+            DrawBlack();            
+        }
     }
 }
